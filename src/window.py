ade3b4d9407e9deb54f2a701ba0ded2187aaaa75
halak0013 <zumruduanka0013@gmail.com>
2023-06-17 22:49:16 +0300

    empty window file

---
+++ src/window.py
@@ -0,0 +1,9 @@
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QApplication, QLabel,QLineEdit,QMessageBox, QSlider, QSplitter, QPushButton, QMainWindow, QHBoxLayout, QWidget, QVBoxLayout, QSizePolicy
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        
