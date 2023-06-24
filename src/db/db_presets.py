f78d579d7853c7df0019c2826b4499a87ba645ac
halak0013 <zumruduanka0013@gmail.com>
2023-06-24 20:20:57 +0300

    db presets

---
+++ src/db/db_presets.py
@@ -0,0 +1,22 @@
+INT_PKA = "INTEGER PRIMARY KEY AUTOINCREMENT"
+INT = "INTEGER"
+TX = "TEXT"
+NN = "NOT NULL"
+
+
+ornek_tablo = [f"id {INT_PKA}", f"name {TX} {NN}"]
+ornek_tablo_c = ["name"]
+
+surah_table = [f"surah_id {INT_PKA}", f"surah_name {TX} {NN}",
+               f"ayat_count {INT}", f"nuzul_sort {INT}"
+               ]
+surah_table_c = ["surah_id", "surah_name", "ayat_count", "nuzul_sort"]
+
+ayat_table = [f"ayat_id {INT_PKA}", f"ayat_no {INT} {NN}",
+              f"ayat {TX}", f"surah_id {INT}", f"is_secde_ayat {INT}",
+              f"page {INT} {NN}", "FOREIGN KEY (surah_id) REFERENCES Surahs(surah_name)"
+              ]
+ayat_table_c = ["ayat_id", "ayat_no", "ayat",
+                "surah_id", "is_secde_ayat", "page"]
+
+secde_
