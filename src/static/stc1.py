83e91a3876a08e6091cb5003890a7bad3b9fd4dc
halak0013 <zumruduanka0013@gmail.com>
2023-07-01 23:41:05 +0300

    adding some static variables for all program

--- src/static/stc1.py
+++ src/static/stc1.py
@@ -1,1 +1,30 @@
-surah_options=["Kuaran-ı Kerim","Alfabetik","Nuzül"]
+from src.db.db_external_pro import Db_Ex_pro
+
+
+
+
+
+#? LIST PART
+surah_options=["Kuaran","Alfabetik","Nuzül"]
+
+
+
+
+
+#? DATABASE PART
+db = Db_Ex_pro("Kuran.db")
+all_ayat_list = db.get_element("Ayat", column="ayat_no,surah_id,page")
+db.end_process()
+del db
+
+
+
+#? SETTING PART
+#TODO: add windows support
+Ayat_f="/tmp/Ayat_Audio"
+
+
+
+#? GLOBAL VARIABLES PART
+crt_Ayat_i=0
+Ayat_dif=0
