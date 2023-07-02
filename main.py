6fa1f5c68eb46e0bc2c2a40d88388f825ffff76c
halak0013 <zumruduanka0013@gmail.com>
2023-07-02 15:00:26 +0300

    window seperating

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-from src.window import MainWindow
-from PySide6.QtGui import QIcon
+from src.window_contine import Window
+from PySide6.QtGui import QIcon, QFont
 from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    app.setFont(QFont("Arial"))
     app.setWindowIcon(QIcon("data/img/logo57_57.png"))
-    mainWindow = MainWindow()
+    mainWindow = Window()
     mainWindow.show()
     sys.exit(app.exec())
