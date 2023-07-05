e4e18388e293e7552d6e944ad54acea5207942b3
halak0013 <zumruduanka0013@gmail.com>
2023-07-05 10:24:00 +0300

    theme and path bug fixing

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import os
+
+print(os.path.dirname(os.path.abspath(__file__)))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
 import sys
 from src.window_contine import Window
+from src.theme import set_custom_theme
 from PySide6.QtGui import QIcon, QFont
 from PySide6.QtWidgets import QApplication
 
@@ -7,6 +13,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setFont(QFont("Arial"))
     app.setWindowIcon(QIcon("data/img/logo57_57.png"))
+    set_custom_theme(app)
     mainWindow = Window()
     mainWindow.show()
     sys.exit(app.exec())
